3b8bd4fed34709d91debe992b5a25f6b71db5352
max <maxyor6040@gmail.com>
2016-03-21 22:56:35 +0200

    backup
    backup

--- app/src/main/java/com/hw1/hw1/MainActivity.java
+++ app/src/main/java/com/hw1/hw1/MainActivity.java
@@ -2,12 +2,63 @@ package com.hw1.hw1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar_num_of_sheep);
+        textView = (TextView) findViewById(R.id.editText_num_of_sheep);
+        textView.setText(String.valueOf(0));
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progress = 0;
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar,
+                                                  int progressValue, boolean fromUser) {
+                        progress = progressValue;
+                        textView.setText(String.valueOf(progressValue));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                });
+        textView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int progress = 0;
+                try {
+                    progress = Integer.valueOf(String.valueOf(s));
+                } catch (NumberFormatException e) {
+
+                }
+                seekBar.setProgress(progress);
+            }
+        });
     }
 }
